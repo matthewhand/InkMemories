73b7145f17fdd7f8c117c299fb4738eaa0228b89
mhand <matthewhandau@gmail.com>
2024-10-06 13:52:18 +1100

    Update setup.sh
    fix(setup.sh): Improve installation process and fix dependencies
    
    * Modified the script to check if EUID is set before checking its value
    * Installed python3-venv and created a virtual environment for the project
    * Installed dependencies using pip in the virtual environment
    * Installed pre-commit and configured it for the repository
    * Fixed the installation of libopenblas-dev to use apt-get with -y flag
    * Hardcoded image source directory, InkMemories root repository, and shared album name for simplicity

--- setup.sh
+++ setup.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-if [ "$EUID" -ne 0 ]; then
+if [ "$EUID" ] && [ "$EUID" -ne 0 ]; then
     echo "This script must be run with sudo."
     exit 1
 fi
@@ -21,12 +21,18 @@ rclone config
 #===========================================================================
 echo "Installing dependencies."
 
-sudo pip install -r displayer_service/requirements.txt
+sudo apt install python3-venv
+python3 -m venv InkMemories
+source InkMemories/bin/activate
+pip list
+pip install -r displayer_service/requirements.txt
+
+echo sudo apt-get -y install pre-commit
 pre-commit install
 
 # Inky throws an error, 'libopenblas.so.0: cannot open shared object file: No such file or directory'.
 # Installing this fixes it.
-sudo apt-get install libopenblas-dev 
+apt-get install -y libopenblas-dev
 
 #===========================================================================
 #                  Configuring Ink Memories to run as a daemon
@@ -35,9 +41,12 @@ echo "Configuring Ink Memories to run as a daemon."
 
 # Interpolate in user-supplied variables into the .service files under
 # /etc/systemd/system.
-read -p "Enter image source directory path (where the image files to display are located at): " image_src_dir
-read -p "Enter the directory of the InkMemories root repository: " ink_memories_root
-read -p "Enter the name of the shared Google Photos album: " shared_album_name
+#read -p "Enter image source directory path (where the image files to display are located at): " image_src_dir
+image_src_dir=source
+#read -p "Enter the directory of the InkMemories root repository: " ink_memories_root
+ink_memories_root=`pwd`
+#read -p "Enter the name of the shared Google Photos album: " shared_album_name
+shared_album_name=CloudPhotos
 
 mkdir -p "${image_src_dir}" 2>&1
 
@@ -48,9 +57,8 @@ for service_file_basename in ink-memories-image-source.service ink-memories-disp
         -e "s|{{SHARED_ALBUM_NAME}}|${shared_album_name}|g" \
         -e "s|{{HOME_DIR}}|${HOME}|g" \
         "./service_files/${service_file_basename}" > /etc/systemd/system/${service_file_basename}
-    
+
     sudo systemctl daemon-reload
     sudo systemctl enable $service_file_basename
     sudo systemctl start $service_file_basename
 done
-
